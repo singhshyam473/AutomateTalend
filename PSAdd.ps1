6f9c3e67ac5810d294041c71636200f2b2e19f5d
Shyam Mohite <shyam.mohite@adrosonic.com>
2020-03-18 17:36:44 +0530

    test

---
+++ PSAdd.ps1
@@ -0,0 +1,12 @@
+$currentUser = New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent())
+$testadmin = $currentUser.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
+if ($testadmin -eq $false) {
+Start-Process powershell.exe -Verb RunAs -ArgumentList ('-noprofile -noexit -file "{0}" -elevated' -f ($myinvocation.MyCommand.Definition))
+exit $LASTEXITCODE
+}
+
+
+$val1 = 1
+$val2 = 2
+$result = $val1 + $val2
+$result
