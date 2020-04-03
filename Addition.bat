d0fcc59a16e4e5b4d28b19973bfebc32252e46c0
Shyam Mohite <shyam.mohite@adrosonic.com>
2020-04-03 15:43:59 +0530

    karan script files

--- Addition.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo Enter two numbers to add
-set /p num1=
-set /p num2=
-set /a res=%num1%+%num2%
-echo result^=%res%
-Pause
--- PSAdd.ps1
+++
@@ -1,8 +0,0 @@
-param($num1,$num2)
-
-[int]$Num1 = [convert]::ToInt32($num1, 10)
-[int]$Num2 = [convert]::ToInt32($num2, 10)
-
-$sum = $num1 + $num2
-
-Write-Host "Result: $sum"
